2eedf45ed152cfbd7590bbfcc5f45785998c4c04
Luciano Xavier <luciano.xavier.lbv@gmail.com>
2019-09-25 09:03:41 -0300

    Alterações 25 09 19

--- index.php
+++ index.php
@@ -35,15 +35,152 @@
             <p>Aumente sua visibilidade com qualidade</p>
             <p>Agência Web especializada Design & Branding, Desenvolvimento Web e Marketing Digital.</p>
         </header>
-        
+
         <main>
             <section class="main_hero container">
                 <div class="content">
-                    <h1>Tenha seu SITE + LOGO <br><span class="font-light">Por apenas</span> R$350,00 <br> (EM ATÉ 96 horas)</h1>
+                    <h1><span class="font-light">Pouco investimento para começar?</span><br>Tenha seu SITE + LOGO <br><span class="font-light">Por apenas</span> <span style="color: #ff0">10x de R$35,00</span> </h1>
+                </div>
+
+                <div class="main_hero_notebook">
+                    <img src="img/lupa-laptop.png" class="responsive-img">
                 </div>
             </section>
+
+            <section class="main_cta container">
+                <div class="row content">
+                    <header>
+                        <i class="fas fa-birthday-cake"></i>
+                        <h1>Promoção imperdível<br> de <span class="color-yellow">aniversário da Lupa</span></h1>
+                        <p>Essa promoção é ideal para aqueles que estão com o orçamento apertado por conta dos gastos iniciais para iniciar seu negócio e gostariam de ter um site com sua própria identidade visual. Esse "empurrãozinho" ajudará a divulgar seus serviços e se tornar reconhecido com seu empreendimento.</p>
+                    </header>
+
+                    <div class="main_cta_icons">
+
+                        <div class="col l6 m6 s12">
+                            <div class="main_cta_icons_round">
+                                <img src="img/lupa-icon-website.jpg" class="responsive-img">
+                            </div>
+                            <ul>
+                                <li><b>Site institucional</b></li>
+                                <li>Com gerenciador de conteúdo</li>
+                                <li>Adaptável aos dispositivos móveis</li>
+                            </ul>
+                        </div>
+
+                        <div class="col l6 m6 s12">
+                            <div class="main_cta_icons_round">
+                                <img src="img/lupa-icon-design.jpg" class="responsive-img">  
+                            </div>
+                            <ul>
+                                <li><b>Logotipo</b></li>
+                                <li>Direitos autorais do cliente</li>
+                                <li>Criado por quem entende</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="main_cta_button_box">
+                        <div class="btn btn-large main_cta_button">Eu quero agora</div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="main_info container">
+                <div class="content">
+                    <header>
+                        <h1>Como funciona?</h1>
+                        <p>Sem pegadinhas. Direto ao ponto!</p>
+                    </header>
+
+                    <div class="main_info_box">
+                        <article>
+                            <img src="img/lupa-icon-hand.jpg" class="responsive-img">
+                            <h2>1º passo:</h2>
+                            <p>Após o pagamento, você preencherá o briefing com o máximo de detalhes possíveis para realizarmos a construção da sua identidade visual. Nesse processo é importante preencher tudo bem detalhado, pois <span style="color: red">não será possível alterar após a criação</span>.</p>
+                        </article>
+
+                        <article>
+                            <img src="img/lupa-icon-canvas.jpg" class="responsive-img">
+                            <h2>2º passo:</h2>
+                            <p>Após o preenchimento, iniciaremos o processo de criação da logo e mandaremos <b>DOIS MODELOS</b> para escolha.</p>
+                        </article>
+
+                        <article>
+                            <img src="img/lupa-icon-award.jpg" class="responsive-img">
+                            <h2>3º passo:</h2>
+                            <p>Com o logotipo aprovado, implementaremos o seu site com a sua marca e informações. Ah… ele irá com Gerenciador de Conteúdo para você mesmo editar suas informações. <b>Veja como será o layout do site: CLIQUE AQUI</b></p>
+                        </article>
+
+                        <div class="main_info_finish">
+                            <p>Tudo pronto! Seu negócio em breve estará online =D</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div class="main_cta_button_box">
+                <div class="btn btn-large main_cta_button">Quero adquirir agora</div>
+            </div>
+
+            <section class="main_faq container">
+                <div class="content">
+                    <header>
+                        <h1>Prováveis dúvidas</h1>
+                    </header>
+
+                    <div class="main_faq_box">
+                        <article>
+                            <h2>Como ficará a hospedagem do meu site?</h2>
+                            <p>Como presente, daremos para você 1 mês de hospedagem gratuita em nossos servidores. <b>Após esse período</b>, o valor será de R$25/mensais mas fica à critério seu continuar ou não.</p>
+                        </article>
+
+                        <article>
+                            <h2>O domínio é gratuito?</h2>
+                            <p>Não! O domínio é o endereço do seu site. O pagamento e a compra dele ficará por conta do cliente. O valor do domínio é anual. Podemos ajudar você nesse processo se não souber.</p>
+                        </article>
+
+                        <article>
+                            <h2>Poderei alterar o logotipo caso não goste?</h2>
+                            <p>Não! Daremos a você duas opções de logo autênticos e sem cópias para você escolher. Caso queira fazer alterações, será cobrado um valor adicional</p>
+                        </article>
+
+                        <article>
+                            <h2>Poderei alterar a estrutura do site?</h2>
+                            <p>Você conseguirá alterar as informações do seu site pelo gerenciador de conteúdo, porém, a estrutura não terá como mexer. Ela será composta por: Topo, banner principal, quem somos, serviços e contato. Caso queira alterar incluíndo algo, será cobrado um valor adicional.</p>
+                        </article>
+
+                        <div class="main_info_finish">
+                            <p>Ainda possui dúvidas? Entre em contato conosco pelo <span class="color-yellow">nosso chat</span> ou pelo e-mail <span class="color-yellow">agencia@lupasolucoes.com</span> que estaremos dispostos a esclarecer tudo</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div class="main_cta_button_box">
+                <div class="btn btn-large main_cta_button">Quero adquirir agora</div>
+            </div>
             
+            <section class="main_about container">
+                <header>
+                    <h1>O que é a LUPA?</h1>
+                </header>
+                <div class="main_about_box content">
+                    <p>Pelo menos uma vez na vida você já utilizou uma lupa por necessidade ou curiosidade para ampliar algo que estava com dificuldades de enxergar. Exatamente esse é o nosso propósito com o seu empreendimento! Fazer que no extenso mundo digital você seja notado com amplitude. Com esse objetivo, foi criada a Lupa Agência Web. Nosso principal foco é dar presença digital ao seu empreendimento proporcionando um Design marcante ligado a criação de site, focando a usabilidade fácil para o usuário e dentro dos padrões exigidos pelos buscadores, e com o Marketing Digital fazendo você realmente ser visto no mundo da web.</p>
+                    <p>Estamos focados em trazer soluções para que o seu investimento se torne em lucro com a provável chegada de clientes. Tudo isso é um trabalho hercúleo mas não impossível, pois com as principais tecnologias no mercado utilizadas pela Lupa, o seu empreendimento estará sempre no mesmo patamar ou a frente dos seus concorrentes.</p>
+                    <div class="main_about_social">
+                        <ul>
+                            <li><a href="https://www.instagram.com/lupaagenciaweb/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="https://www.facebook.com/lupaagenciaweb/" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
         </main>
+        
+        <footer class="main_footer container">
+            <p>© <a href="#" target="_blank">Lupa Agência Web</a> - Todos os direitos reservados</p>
+        </footer>
 
         <script src="_cdn/js/jquery-3.3.1.min.js" type="text/javascript"></script>
         <script src="_cdn/js/main.js" type="text/javascript"></script>
