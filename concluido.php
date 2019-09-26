6cbcfee5f581506f4dcde54a00040efdddaa4018
Luciano Xavier <luciano.xavier.lbv@gmail.com>
2019-09-26 18:10:53 -0300

    Criado página de conclusão

---
+++ concluido.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1, user-scalable=no">
+        <title>Promoção AMPLIAR - Mês de aniversário da LUPA </title>
+        <meta name="description" content="Essa promoção é ideal para aqueles que estão com o orçamento apertado por conta dos gastos iniciais para iniciar seu negócio e gostariam de ter um site com sua própria marca. Esse empurrãozinho ajudará a divulgar seus serviços e se tornar reconhecido com seu empreendimento."/>
+        <meta name="robots" content="index, follow"/>
+
+        <link rel="canonical" href="https://lupasolucoes.com/ampliar">
+
+        <meta property="og:type" content="article"/>
+        <meta property="og:title" content="Tenha seu logo e site com orçamento apertado"/>
+        <meta property="og:description" content="Essa promoção é ideal para aqueles que estão com o orçamento apertado por conta dos gastos iniciais para iniciar seu negócio e gostariam de ter um site com sua própria marca. Esse empurrãozinho ajudará a divulgar seus serviços e se tornar reconhecido com seu empreendimento."/>
+        <meta property="og:image" content="https://lupasolucoes.com/ampliar/img/lupa-social.png"/>
+        <meta property="og:url" content="https://lupasolucoes.com/ampliar"/>
+        <meta property="og:site_name" content="Promoção AMPLIAR - Crie seu logo e site"/>
+        <meta property="og:locale" content="pt-br"/>
+
+        <meta itemprop="name" content="Tenha seu logo e site com orçamento apertado"/>
+        <meta itemprop="description" content="Essa promoção é ideal para aqueles que estão com o orçamento apertado por conta dos gastos iniciais para iniciar seu negócio e gostariam de ter um site com sua própria marca. Esse empurrãozinho ajudará a divulgar seus serviços e se tornar reconhecido com seu empreendimento."/>
+        <meta itemprop="image" content="https://lupasolucoes.com/ampliar/img/lupa-social.png"/>
+        <meta itemprop="url" content="https://lupasolucoes.com/ampliar"/>
+
+        <link rel="shortcut icon" href="img/favicon.png" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">        
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="_cdn/css/materialize.css"/>
+        <link rel="stylesheet" href="_cdn/css/unslider.css"/>
+        <link rel="stylesheet" href="_cdn/css/unslider-dots.css"/>
+        <link rel="stylesheet" href="_cdn/js/slick/slick-theme.css"/>
+        <link rel="stylesheet" href="_cdn/js/slick/slick.css"/>
+
+        <script src="//code.jivosite.com/widget.js" data-jv-id="SiGZRAbtgz" async></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148856021-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-148856021-1');
+        </script>
+    </head>
+    <body>
+        <header class="main_header container">
+            <img src="img/logo-lupa.jpg.png" class="responsive-img" alt="Lupa Soluções - Agência Web especializada Design & Branding, Desenvolvimento Web e Marketing Digital." title="Lupa Soluções - Agência Web especializada Design & Branding, Desenvolvimento Web e Marketing Digital.">
+            <p>Aumente sua visibilidade com qualidade</p>
+            <p>Agência Web especializada Design & Branding, Desenvolvimento Web e Marketing Digital.</p>
+        </header>
+
+        <main>
+            <section class="main_hero container">
+                <div class="content">
+                    <h1><span class="font-light">PAGAMENTO EFETUADO COM SUCESSO! <br> Agora preencha o briefing clicando abaixo </h1>
+                </div>
+
+                <div class="main_cta_button_box">
+                    <div class="btn btn-large main_cta_button"><a href="https://forms.gle/UTeqwaYBEYykmBsK7">Preencher briefing</a></div>
+                </div>
+            </section>
+
+            <section class="main_about container">
+                <header>
+                    <h1>O que é a LUPA?</h1>
+                </header>
+                <div class="main_about_box content">
+                    <p>Pelo menos uma vez na vida você já utilizou uma lupa por necessidade ou curiosidade para ampliar algo que estava com dificuldades de enxergar. Exatamente esse é o nosso propósito com o seu empreendimento! Fazer que no extenso mundo digital você seja notado com amplitude. Com esse objetivo, foi criada a Lupa Agência Web. Nosso principal foco é dar presença digital ao seu empreendimento proporcionando um Design marcante ligado a criação de site, focando a usabilidade fácil para o usuário e dentro dos padrões exigidos pelos buscadores, e com o Marketing Digital fazendo você realmente ser visto no mundo da web.</p>
+                    <p>Estamos focados em trazer soluções para que o seu investimento se torne em lucro com a provável chegada de clientes. Tudo isso é um trabalho hercúleo mas não impossível, pois com as principais tecnologias no mercado utilizadas pela Lupa, o seu empreendimento estará sempre no mesmo patamar ou a frente dos seus concorrentes.</p>
+                    <div class="main_about_social">
+                        <ul>
+                            <li><a href="https://www.instagram.com/lupaagenciaweb/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="https://www.facebook.com/lupaagenciaweb/" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+            <section class="main_testemonial boxs container">
+                <div class="content row">
+                    <h1>Quem conhece <span style="font-weight: 700">aprova!</span></h1>
+
+                    <div class="slider_testemonial col s12">
+                        <article>
+                            <div class="main_testemonial_photo col s12 m4 l4">
+                                <div class="mask-photos">
+                                    <img src="img/margareth.jpg" alt="" title="" class="responsive-img"/>
+                                </div>
+                            </div>
+                            <div class="main_testemonial_text col s12 m8 l8">
+                                <h1>Margareth Murteira - Coach</h1>
+                                <p>"Quando competência se junta com espiritualidade, boa vontade e respeito, fazemos uma equipe integral! Com a Lupa Soluções é assim. Obrigada pela parceria."</p>
+                            </div>
+                        </article>
+
+                        <article>
+                            <div class="main_testemonial_photo col s12 m4 l4">
+                                <div class="mask-photos">
+                                    <img src="img/marcos.jpg" alt="" title="" class="responsive-img"/>
+                                </div>
+                            </div>
+                            <div class="main_testemonial_text col s12 m8 l8">
+                                <h1>Marcos Costa - Músico</h1>
+                                <p>"Excelentes profissionais que entregam com qualidade o que prometem. Se eles atendem o que você precisa, pode ir e confiar!"</p>
+                            </div>
+                        </article>
+
+                        <article>
+                            <div class="main_testemonial_photo col s12 m4 l4">
+                                <div class="mask-photos">
+                                    <img src="img/matheus.jpg" alt="" title="" class="responsive-img"/>
+                                </div>
+                            </div>
+                            <div class="main_testemonial_text col s12 m8 l8">
+                                <h1>Matheus Ribeiro - Produtor Musical</h1>
+                                <p>"Com a Lupa Soluções é preto no branco. Seriedade, amizade e comprometimento fazem parte de uma história longa de parceria"</p>
+                            </div>
+                        </article>
+
+                        <article>
+                            <div class="main_testemonial_photo col s12 m4 l4">
+                                <div class="mask-photos">
+                                    <img src="img/deyse.jpg" alt="" title="" class="responsive-img"/>
+                                </div>
+                            </div>
+                            <div class="main_testemonial_text col s12 m8 l8">
+                                <h1>Deyse Pereira - Psicóloga</h1>
+                                <p>"Parceria totalmente fundada na lealdade e no compromisso. Com a Lupa Soluções temos a garantia de que nosso investimento não é em vão."</p>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <footer class="main_footer container">
+            <p>© <a href="#" target="_blank">Lupa Agência Web</a> - Todos os direitos reservados</p>
+        </footer>
+
+        <script src="_cdn/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.lightbox.js"></script>
+        <script src="_cdn/js/slick/slick.min.js" type="text/javascript"></script>
+        <script src="_cdn/js/scrollreveal.min.js" type="text/javascript"></script>
+        <script src="_cdn/js/main.js" type="text/javascript"></script>
+        <script src="_cdn/js/materialize.js" type="text/javascript"></script>
+        <script src="_cdn/js/unslider.js" type="text/javascript"></script>
+    </body>
+</html>
